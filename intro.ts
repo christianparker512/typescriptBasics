3791ea3f1e363b6dd313bf76f29f43332d2c41e7
Christian Parker <christianparker512@gmail.com>
2021-08-30 14:09:21 -0500

    loops in typescript

--- intro.ts
+++ intro.ts
@@ -1,25 +1,3 @@
-// let a: number = 100;
-// if (a > 50) {
-//   console.log('a is greater than 50');
-// } else {
-//   console.log('a is not greater than 50');
-// }
-
-let a: number = 100;
-switch (a) {
-  case 1:
-    console.log('Value is 1');
-    break;
-    case 10:
-    console.log('Value is 10');
-    break;
-    case 100:
-    console.log('Value is 100');
-    break;
-    case 1000:
-    console.log('Value is 1000');
-    break;
-    default:
-    console.log('Value is something else');
-    break;
+for (let i = 0; i < 10; i++){
+  console.log(i * i);
 }
