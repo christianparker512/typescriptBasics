622628a7a92ad8202964a210d1e6664d04811d4d
Christian Parker <christianparker512@gmail.com>
2021-08-30 13:34:12 -0500

    data types

--- intro.ts
+++ intro.ts
@@ -3,3 +3,15 @@ var b: string = "Christian";
 var c: boolean = true;
 console.log(a);
 
+
+var d: number = 20;
+
+let e: number = 20;
+
+const f: number = 30;
+
+
+let g: string = "parker";
+// g = 10;
+
+console.log(g);
