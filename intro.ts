398e48b5a861e1005733c867aa1a9ee1e59668e3
Christian Parker <christianparker512@gmail.com>
2021-08-30 14:13:41 -0500

    while loops

--- intro.ts
+++ intro.ts
@@ -3,6 +3,26 @@ for(let i = 0; i < 10; i++) {
 }
 
 let arr = [10, 20, 30, 40, 50, 60]
+
+//for of statements
 for (let elem of arr) {
   console.log(elem);
 }
+
+// prints the value of the array
+let arr1 = 'Christian Parker'
+for (let elem of arr1) {
+  console.log(elem);
+}
+// prints the index value
+let arr2 = 'Christian';
+for (let elem in arr2) {
+  console.log(elem);
+}
+
+let a: number = 10;
+while(a<20) {
+  console.log(a);
+  a++;
+}
+
