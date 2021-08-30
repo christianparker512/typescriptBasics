3c63b5200b03ca946b86e693601db10866203491
Christian Parker <christianparker512@gmail.com>
2021-08-30 14:08:04 -0500

    conditional statements if else and switch statements

--- intro.ts
+++ intro.ts
@@ -1,17 +1,25 @@
+// let a: number = 100;
+// if (a > 50) {
+//   console.log('a is greater than 50');
+// } else {
+//   console.log('a is not greater than 50');
+// }
+
 let a: number = 100;
-// a = a % 2;
-console.log(a++);
-//10
-console.log(a);
-//11
-
-console.log(a > 50 ? 'christian' : 'parker');
-
-// let c: number = 100;
-// let d: string = '100';
-
-let c = 100;
-let d = '100';
-// console.log(c == d);
-
-console.log(a>10 && a>200 ? 'true' : 'false' )
+switch (a) {
+  case 1:
+    console.log('Value is 1');
+    break;
+    case 10:
+    console.log('Value is 10');
+    break;
+    case 100:
+    console.log('Value is 100');
+    break;
+    case 1000:
+    console.log('Value is 1000');
+    break;
+    default:
+    console.log('Value is something else');
+    break;
+}
