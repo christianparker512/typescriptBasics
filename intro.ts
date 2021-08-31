8e924e3f2adf7308cd808e65d02959599df414bb
Christian Parker <christianparker512@gmail.com>
2021-08-30 19:57:35 -0500

    random number functions

--- intro.ts
+++ intro.ts
@@ -1,33 +1,16 @@
-for(let i = 0; i < 10; i++) {
-  console.log(i * i);
+function sayHello() {
+  console.log("hello Christian");
+  console.log("hello Lisa");
+  console.log("hello Abby");
 }
 
-let arr = [10, 20, 30, 40, 50, 60]
-
-//for of statements
-for (let elem of arr) {
-  console.log(elem);
+function sumOfTwo(x: number,y: number) {
+  console.log(x + y);
 }
+sumOfTwo(10, 20);
 
-// prints the value of the array
-let arr1 = 'Christian Parker'
-for (let elem of arr1) {
-  console.log(elem);
+function getRandomNumber(): number{
+  return Math.random();
 }
-// prints the index value
-let arr2 = 'Christian';
-for (let eleme in arr2) {
-  console.log(eleme);
-}
-
-let a: number = 10;
-while(a<20) {
-  console.log(a);
-  a++;
-}
-
-let h: number = 10;
-do {
-  console.log(h);
-  h++;
-} while (a<20)
+let a = getRandomNumber();
+console.log(a);
