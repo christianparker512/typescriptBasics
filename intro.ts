d0d264cbccf3cfb5c35e88283711140c1288325f
Christian Parker <christianparker512@gmail.com>
2021-08-30 20:17:26 -0500

    functions with arrays as parrameters

--- intro.ts
+++ intro.ts
@@ -23,3 +23,15 @@ function getSum(a: number, b: number , c? : number): number {
 // let ans = getSum(10, 100);
 let ans = getSum(10, 20, 300);
 console.log(ans);
+
+function getSum2(...nums: number[]): number {
+  let ans: number = 0;
+  for (let num of nums) {
+    ans = ans + num
+  }
+  return ans;
+}
+console.log(getSum2(1, 2));
+console.log(getSum2(1, 2, 3));
+console.log(getSum2(1, 2, 3,4,5,6));
+console.log(getSum2(1, 2,3,4,5,6,7,8));
