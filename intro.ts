46819eab9887ef0e2e82d5a55da4b5a4b74b2983
Christian Parker <christianparker512@gmail.com>
2021-09-01 10:08:37 -0500

    changes

--- intro.ts
+++ intro.ts
@@ -1,37 +1,7 @@
-function sayHello() {
-  console.log("hello Christian");
-  console.log("hello Lisa");
-  console.log("hello Abby");
-}
 
-function sumOfTwo(x: number,y: number) {
-  console.log(x + y);
-}
-sumOfTwo(10, 20);
+//anonymous eample
+let result = function (a: number, b: number) { return a * b; }
+console.log(result(10, 20));
 
-function getRandomNumber(): number{
-  return Math.random();
-}
-let a = getRandomNumber();
-console.log(a);
-
-function getSum(a: number, b: number , c? : number): number {
-  // return a + b;
-  if (c != undefined) { return a + b + c }
-  return a + b;
-} 
-// let ans = getSum(10, 100);
-let ans = getSum(10, 20, 300);
-console.log(ans);
-
-function getSum2(...nums: number[]): number {
-  let ans: number = 0;
-  for (let num of nums) {
-    ans = ans + num
-  }
-  return ans;
-}
-console.log(getSum2(1, 2));
-console.log(getSum2(1, 2, 3));
-console.log(getSum2(1, 2, 3,4,5,6));
-console.log(getSum2(1, 2,3,4,5,6,7,8));
+//lambda
+var msg = () => { console.log('Hello Kamal!');}
